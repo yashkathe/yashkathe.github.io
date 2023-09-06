85d64205eff61d33ca23a054d3281527700a42a2
Yash Kathe <yashkathe123@gmail.com>
2023-09-06 13:18:47 +0530

    added temporary pfp instead of 3d model

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
-import ComputerCanvas from "./Components/DesktopModel";
-import UnderDevelopmentMessage from "../Extra/UnderDev";
+// import ComputerCanvas from "./Components/DesktopModel";
+// import UnderDevelopmentMessage from "../Extra/UnderDev";
 import Intro from "./Components/Intro";
 
 import styles from "./Home.module.css";
 
+import pfp from "/profile-picture.jpg";
+
+import VarientStore from "../Store/VarientStore";
+
 const Home = () => {
+	const variantsCtx = useContext(VarientStore);
+
 	return (
 		<div className={styles.parent}>
 			<div>
 				<Intro />
 			</div>
-			<div>{/* <ComputerCanvas /> */}</div>
+			<div>
+				<motion.div
+					className={styles.profile}
+					variants={
+						window.innerWidth < 896
+							? variantsCtx.mobilePop
+							: variantsCtx.rightSlideIn
+					}
+					initial='initial'
+					animate='animate'>
+					<img src={pfp} alt='pfp' />
+				</motion.div>
+			</div>
 		</div>
 	);
 };
